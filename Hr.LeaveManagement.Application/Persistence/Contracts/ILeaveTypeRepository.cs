f049056edb20e2b654c1c04fcd9eebab709c5dd8
vasiliv <32122468+vasiliv@users.noreply.github.com>
2022-12-23 19:52:07 +0400

    2-6 finished

---
+++ Hr.LeaveManagement.Application/Persistence/Contracts/ILeaveTypeRepository.cs
@@ -0,0 +1,11 @@
+﻿using Hr.LeaveManagement.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hr.LeaveManagement.Application.Persistence.Contracts
+{
+    public interface ILeaveTypeRepository : IGenericRepository<LeaveType>
+    {
+    }
+}
